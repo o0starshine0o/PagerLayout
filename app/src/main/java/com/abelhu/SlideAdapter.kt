663b86984ff40e7a6ca5c12c743fa30bafce93b1
huyongsheng <huyongsheng@xianlai.com>
2020-01-20 19:14:56 +0800

    add : debug build type

--- app/src/main/java/com/abelhu/SlideAdapter.kt
+++ app/src/main/java/com/abelhu/SlideAdapter.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.abelhu.lockitem.LockItem
 import kotlinx.android.synthetic.main.item_icon.view.*
 
