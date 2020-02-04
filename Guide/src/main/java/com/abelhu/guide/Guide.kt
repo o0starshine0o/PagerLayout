f80f959b5066eb5ae8472984a3c13680f0ef4a3c
huyongsheng <huyongsheng@xianlai.com>
2020-02-04 17:55:38 +0800

    add : guide

--- Guide/src/main/java/com/abelhu/guide/Guide.kt
+++ Guide/src/main/java/com/abelhu/guide/Guide.kt
@@ -1,23 +1,40 @@
 package com.abelhu.guide
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.ColorDrawable
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import com.abelhu.guide.Guide.LayoutParams.Companion.TYPE_STAY
+import com.abelhu.guide.Guide.LayoutParams.Companion.TYPE_WINDOW
+import java.util.*
 
 class Guide @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ConstraintLayout(context, attrs, defStyleAttr) {
+    companion object {
+        private val Tag = Guide::class.java.simpleName
+    }
+
     /**
      * 画笔
      */
     private var paint = Paint()
+    /**
+     * 需要扣除的窗口
+     */
+    private var windows = LinkedList<Window>()
+    /**
+     * 缓存背景
+     */
+    private var backgroundBitmap: Bitmap? = null
 
-    var window: View? = null
-    var windowBitmap: Bitmap? = null
-
-    var backgroundBitmap: Bitmap? = null
+    /**
+     * 除去必要的空间，此控件内的子控件都需要移除
+     */
+    private val removeList = LinkedList<View>()
 
     init {
         // 获取定义属性
@@ -54,10 +71,6 @@ class Guide @JvmOverloads constructor(context: Context, attrs: AttributeSet? = n
             // background 设置为空，拦截背景的绘制
             background = ColorDrawable(Color.TRANSPARENT)
         }
-        // 窗口区域使用新的bitmap
-        window?.also {
-            if (windowBitmap == null) windowBitmap = Bitmap.createBitmap(it.measuredWidth, it.measuredHeight, Bitmap.Config.ARGB_8888)
-        }
         super.draw(canvas)
     }
 
@@ -66,17 +79,23 @@ class Guide @JvmOverloads constructor(context: Context, attrs: AttributeSet? = n
         drawBackWithWindow(canvas)
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, left, top, right, bottom)
         // 获取最后一个标记为touchWindow的child，设置其为window
-        if (window == null && childCount > 0) {
-            for (i in 0 until childCount) {
-                if ((getChildAt(i).layoutParams as LayoutParams).touchWindow) window = getChildAt(i)
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            when ((child.layoutParams as LayoutParams).type) {
+                TYPE_WINDOW -> {
+                    windows.add(Window(child, intArrayOf(0, 0)))
+                    removeList.add(child)
+                }
+                TYPE_STAY -> Log.i(Tag, "stay child")
+                else -> removeList.add(child)
             }
         }
-        super.onLayout(changed, left, top, right, bottom)
-        // 移除所有view
-        removeAllViews()
-        //
+        // 移除所有要被移除的children
+        for (view in removeList) removeView(view)
     }
 
     @Suppress("DEPRECATION")
@@ -87,22 +106,41 @@ class Guide @JvmOverloads constructor(context: Context, attrs: AttributeSet? = n
         backgroundBitmap?.also { canvas.drawBitmap(it, 0f, 0f, paint) }
         // 去掉窗口区域
         paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
-        val location = intArrayOf(0, 0)
-        window?.getLocationInWindow(location)
-        windowBitmap?.also { canvas.drawBitmap(it, location[0].toFloat(), location[1].toFloat(), paint) }
+        for (window in windows) {
+            window.bitmap?.also { canvas.drawBitmap(it, window.left, window.top, paint) }
+        }
         paint.xfermode = null
         // 应用图层
         canvas.restoreToCount(layer)
     }
 
+    class Window(view: View, private val location: IntArray) {
+        var bitmap: Bitmap? = null
+        val left
+            get() = location[0].toFloat()
+        val top
+            get() = location[1].toFloat()
+
+        init {
+            view.getLocationInWindow(location)
+            bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        }
+    }
+
     class LayoutParams : ConstraintLayout.LayoutParams {
-        var touchWindow = false
+        companion object {
+            const val TYPE_OTHER = 0
+            const val TYPE_WINDOW = 1
+            const val TYPE_STAY = 2
+        }
+
+        var type = TYPE_OTHER
 
         constructor(source: ViewGroup.LayoutParams) : super(source)
 
         constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
             val typedArray = context.obtainStyledAttributes(attrs, R.styleable.Guide_Layout)
-            touchWindow = typedArray.getBoolean(R.styleable.Guide_Layout_layout_touchWindow, touchWindow)
+            type = typedArray.getInt(R.styleable.Guide_Layout_layout_type, TYPE_OTHER)
             typedArray.recycle()
         }
     }
